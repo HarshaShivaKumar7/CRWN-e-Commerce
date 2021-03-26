3e3047022749a8164d43c5be79ec9e455837d1e5
Harsha ShivaKumar <hsharshahsnckm@gmail.com>
2021-03-26 18:01:04 +0530

    Updated with adding React Router for each Itema....

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import HomePage from "./Pages/HomePage/HomePage";
 import "./App.css";
-import HomePage from "./HomePage/HomePage";
+
+const Hats = () => {
+    return (
+        <div>
+            <h1 style={{ textAlign: "center" }}>HATS</h1>
+        </div>
+    );
+};
+const Jackets = () => {
+    return (
+        <div>
+            <h1 style={{ textAlign: "center" }}>JACKETS</h1>
+        </div>
+    );
+};
+const Sneakers = () => {
+    return (
+        <div>
+            <h1 style={{ textAlign: "center" }}>SNEAKERS</h1>
+        </div>
+    );
+};
+const Womens = () => {
+    return (
+        <div>
+            <h1 style={{ textAlign: "center" }}>WOMENS</h1>
+        </div>
+    );
+};
+const Mens = () => {
+    return (
+        <div>
+            <h1 style={{ textAlign: "center" }}>MENS</h1>
+        </div>
+    );
+};
+
 function App() {
     return (
         <div>
-            <HomePage />
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" component={HomePage} />
+                    <Route path="/hats" component={Hats} />
+                    <Route path="/jackets" component={Jackets} />
+                    <Route path="/sneakers" component={Sneakers} />
+                    <Route path="/womens" component={Womens} />
+                    <Route path="/mens" component={Mens} />
+                </Switch>
+            </BrowserRouter>
         </div>
     );
 }
---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HomePage.scss";
+import Directory from "../../Components/Directory/Directory";
+const HomePage = () => {
+    return (
+        <div className="homepage">
+            <Directory />
+        </div>
+    );
+};
+
+export default HomePage;
